8bb8eab343f08f00bc30e572382ba74f9e5ebbf0
Espen Gjærde <Espen Gjrde>
2016-09-13 21:09:46 +0200

    ok done

--- recieve_board.c
+++ recieve_board.c
@@ -20,29 +20,26 @@ void setup(void){
  
 void loop(void){
 	if (radio.available()){
-	   Serial.println("Radio Available");
-	   bool done = false;   
-	   while (!done){
-		   done = radio.read(msg, 1);
-		   Serial.println("Checking button");
-		   Serial.println(msg[0]);
-		   if (msg[0] == 111 || msg[0] == 222){
-				Serial.println("message recieved");
-				if(msg[0] == 111){
-					Serial.println("Recieved from button board");
-					Serial.println("Setting led 48 on");
-					digitalWrite(LED1, HIGH);
-				}
-				if(msg[0] == 222 ) {
-					Serial.println("Recieved from motion detector");
-					Serial.println("Setting led 53 on");
-				}
-		   }
-		   else {
-				System.println("No motion or button push");
-				digitalWrite(LED1, LOW);
-				Serial.println("Turn off");
-		   }
+	   Serial.println("Radio Available");   
+	   Serial.println("Checking button and motion boards");
+	   Serial.println(msg[0]);
+	   if (msg[0] == 111 || msg[0] == 222){
+			Serial.println("message recieved");
+			if(msg[0] == 111){
+				Serial.println("Recieved from button board");
+				Serial.println("Setting led 48 on");
+				digitalWrite(LED1, HIGH);
+			}
+			if(msg[0] == 222 ) {
+				Serial.println("Recieved from motion detector");
+				Serial.println("Setting led 53 on");
+				digitalWrite(LED2, HIGH);
+			}
+	   }
+	   else {
+			System.println("No motion or button push");
+			digitalWrite(LED1, LOW);
+			Serial.println("Turn off");
 	   }
 	}
 	else {
