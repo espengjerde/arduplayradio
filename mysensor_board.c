65bacb793d382208ae3ca69cd46437ba675033e9
Espen Gjærde <espen.gjerde@gmail.com>
2016-10-11 19:06:58 +0200

    commented out all Serial

--- mysensor_board.c
+++ mysensor_board.c
@@ -15,7 +15,7 @@ MySensor gw;
 
 
 void setup(void){
-	Serial.begin(9600);
+//	Serial.begin(9600);
     gw.begin(incomingMessage, Auto, true);
 	radio.begin();
 	radio.openReadingPipe(1,pipe);
@@ -27,38 +27,38 @@ void setup(void){
  
 void loop(void){
 	if (radio.available()){
-	   Serial.println("Radio Available");   
+//	   Serial.println("Radio Available");   
 	   radio.read(msg, 1);
 	   
-	   Serial.println("Checking button and motion boards");
-	   Serial.println(msg[0]);
+//	   Serial.println("Checking button and motion boards");
+//	   Serial.println(msg[0]);
 	   
 	   if (msg[0] == 111 || msg[0] == 222){
-			Serial.println("message recieved");
+//			Serial.println("message recieved");
 			if(msg[0] == 111){
-				Serial.println("Recieved from button board");
-				Serial.println("Setting Button LED on");
+//				Serial.println("Recieved from button board");
+//				Serial.println("Setting Button LED on");
 				digitalWrite(LED_B, HIGH);
 			}
 			if(msg[0] == 222 ) {
-				Serial.println("Recieved from motion detector");
-				Serial.println("Setting Motion LED on");
+//				Serial.println("Recieved from motion detector");
+//				Serial.println("Setting Motion LED on");
 				digitalWrite(LED_M, HIGH);
 			}
 	   }
 	   else {
-			Serial.println("No motion or button push");
+//			Serial.println("No motion or button push");
 			if(msg[0] == 221){
 				digitalWrite(LED_M, LOW);
 				Serial.println("Turn off Motion LED");
 			}
 			if(msg[0] == 100){
 				digitalWrite(LED_B, LOW);
-				Serial.println("Turn off Button LED");
+//				Serial.println("Turn off Button LED");
 			}
 	   }
 	}
 	else {
-	   Serial.println("No radio available");
+//	   Serial.println("No radio available");
 	}
 }
