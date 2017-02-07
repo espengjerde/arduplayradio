d7eddd78b20dcb955a91aba07f500faebacd1d3b
espengjerde <espengjerde@github.com>
2017-02-07 21:02:05 +0100

    recieve actual temp

---
+++ recieve_temp2.ino
@@ -0,0 +1,56 @@
+//recieve both;
+
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+float msg[1];
+RF24 radio(9,53);
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+int LED_B = 48;
+int LED_M = 49;
+ 
+void setup(void){
+	Serial.begin(9600);
+	radio.begin();
+	radio.openReadingPipe(1,pipe);
+	radio.startListening();
+	pinMode(LED_B, OUTPUT);
+	pinMode(LED_M, OUTPUT);
+}
+ 
+void loop(void){
+	if (radio.available()){
+	   Serial.println("Radio Available");   
+	   radio.read(msg, 1);
+	   
+	   Serial.println("Reading temperature");
+	   Serial.println(msg[0]);
+       digitalWrite(LED_M, LOW);
+       digitalWrite(LED_B, LOW);   
+    
+    if(msg[0] < 15.0){
+        Serial.print("YELLOW");
+        digitalWrite(LED_M,LOW);
+        digitalWrite(LED_B,HIGH);
+        delay(500);
+        return;
+    }
+    if(msg[0] < 20.0){
+        Serial.print("YELLOW_AND_RED");
+        digitalWrite(LED_M,HIGH);
+        digitalWrite(LED_B,HIGH);
+        delay(500);
+        return;
+    }
+    if(msg[0] > 35.0){
+        Serial.print("RED");
+        digitalWrite(LED_M,HIGH);
+        digitalWrite(LED_B,LOW);
+        delay(500);
+        return;
+    }
+	}
+	else {
+	   Serial.println("No radio available");
+    }
+}
