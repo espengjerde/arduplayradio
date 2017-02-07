8a8cf140e26c7527db6efd2db834c181a002d21d
espengjerde <espengjerde@github.com>
2017-02-07 21:57:09 +0100

    lalala

--- recieve_temp2.ino
+++ recieve_temp2.ino
@@ -3,12 +3,11 @@
 #include <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
-float msg[1];
+float msg;
 RF24 radio(9,53);
 const uint64_t pipe = 0xE8E8F0F0E1LL;
 int LED_B = 48;
 int LED_M = 49;
-int status = -2; 
 void setup(void){
 	Serial.begin(9600);
 	radio.begin();
@@ -24,25 +23,25 @@ void loop(void){
 	   status = radio.read(msg, 4); 
 	   Serial.println("Reading temperature");
        Serial.println(status);
-	   Serial.println(msg[0]);
+	   Serial.println(msg);
        digitalWrite(LED_M, LOW);
        digitalWrite(LED_B, LOW);   
     
-    if(msg[0] < 15.0){
+    if(msg < 15.0){
         Serial.print("YELLOW");
         digitalWrite(LED_M,LOW);
         digitalWrite(LED_B,HIGH);
         delay(500);
         return;
     }
-    if(msg[0] < 20.0){
+    if(msg < 20.0){
         Serial.print("YELLOW_AND_RED");
         digitalWrite(LED_M,HIGH);
         digitalWrite(LED_B,HIGH);
         delay(500);
         return;
     }
-    if(msg[0] > 35.0){
+    if(msg > 35.0){
         Serial.print("RED");
         digitalWrite(LED_M,HIGH);
         digitalWrite(LED_B,LOW);
--- transmitt_temp_simple.ino
+++ transmitt_temp_simple.ino
@@ -8,10 +8,9 @@
 
 DHT dht(DHTPIN,DHTTYPE);
 
-float msg[1];  
+float msg;  
 RF24 radio(9,10);
 const uint64_t pipe = 0xE8E8F0F0E1LL;
-int status = -2;
 
 void setup() {
 	Serial.begin(9600);
@@ -21,9 +20,7 @@ void setup() {
 }
  
 void loop(){
-        msg[0] = dht.readTemperature();	
-        Serial.println(msg[0]);
-    	status = radio.write(msg, 4);
-        Serial.println(status);
-        delay(200);
+        msg = dht.readTemperature();	
+        Serial.println(msg);
+        radio.write(msg, 4);
 }
