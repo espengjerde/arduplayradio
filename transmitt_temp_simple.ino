8bfd3f05990c42da5e12177fc12a0ab6b414f2e2
espengjerde <espengjerde@github.com>
2017-02-07 20:47:05 +0100

    send-temp-direct

---
+++ transmitt_temp_simple.ino
@@ -0,0 +1,30 @@
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+#include <dht.h>
+
+#define DHTPIN 7
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN,DHTTYPE);
+
+float msg[1];  
+RF24 radio(9,10);
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+
+
+void setup() {
+	Serial.begin(9600);
+	radio.begin();
+	radio.openWritingPipe(pipe);
+	radio.stopListening ();
+}
+ 
+void loop(){
+    float temp = dht.readTemperature();	
+		msg[0] = temp;
+		radio.write(msg, 1);
+        Serial.println(temp);
+    }
+    delay(1000);
+}
